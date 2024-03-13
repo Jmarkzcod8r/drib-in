d8cc9b028647106d345bc3bc42061656ab3ab0c4
Jmarkzcod8r <jmgutierrez122091@gmail.com>
2024-03-13 20:55:28 +0800

    commented out profile in middleware which seems to take out profile page on protectedPaths

--- middleware.ts
+++ middleware.ts
@@ -24,7 +24,7 @@ const authConfig = {
         /\/shipping/,
         /\/payment/,
         /\/place-order/,
-        /\/profile/,
+        // /\/profile/,
         /\/order\/(.*)/,
         /\/admin/,
       ]
