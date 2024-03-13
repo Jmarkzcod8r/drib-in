905470a8c3456094cb80bedc0dcf1c3c3a67daaa
Jmarkzcod8r <jmgutierrez122091@gmail.com>
2024-03-13 21:07:15 +0800

    In api/products/seed/route.ts -> adjust code to leave the mongodb as is

--- app/api/products/seed/route.ts
+++ app/api/products/seed/route.ts
@@ -7,11 +7,11 @@ import { NextRequest, NextResponse } from 'next/server'
 export const GET = async (request: NextRequest) => {
   const { users, products } = data
   await dbConnect()
-  await UserModel.deleteMany()
-  await UserModel.insertMany(users)
+  // await UserModel.deleteMany()
+  // await UserModel.insertMany(users)
 
-  await ProductModel.deleteMany()
-  await ProductModel.insertMany(products)
+  // await ProductModel.deleteMany()
+  // await ProductModel.insertMany(products)
 
   return NextResponse.json({
     message: 'seeded successfully',
