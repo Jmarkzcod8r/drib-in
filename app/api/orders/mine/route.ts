80587e6fe1bc25fd01ed219133f8444fbdd92260
Jmarkzcod8r <jmgutierrez122091@gmail.com>
2024-03-19 03:04:00 +0800

    Remove auth requirement for api/orders/mine

--- app/api/orders/mine/route.ts
+++ app/api/orders/mine/route.ts
@@ -5,14 +5,14 @@ import OrderModel from '@/lib/models/OrderModel'
 import { auth } from '@/lib/auth'
 
 export const GET = auth(async (req: any) => {
-  if (!req.auth) {
-    return Response.json(
-      { message: 'unauthorized' },
-      {
-        status: 401,
-      }
-    )
-  }
+  // if (!req.auth) {
+  //   return Response.json(
+  //     { message: 'unauthorized' },
+  //     {
+  //       status: 401,
+  //     }
+  //   )
+  // }
   const { user } = req.auth
   await dbConnect()
   const orders = await OrderModel.find({ user: user._id })
